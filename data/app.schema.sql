f7d531de22929df91f2ea8d71e11894872daf87e
Ayrton Gomes <com.ayrton@gmail.com>
2017-07-20 02:12:05 -0100

    app sql file

---
+++ data/app.schema.sql
@@ -0,0 +1,54 @@
+create table orcamento (
+    id BIGINT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    titulo varchar(50) NOT NULL,
+    descricao text,
+    valor DECIMAL(10, 0),
+
+    data_criacao datetime NOT NULL default CURRENT_DATE,
+    data_update datetime NOT NULL default CURRENT_DATE,
+    criado_por BIGINT(20),
+    updated_por BIGINT(20)
+);
+
+create table factura (
+    id BIGINT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    titulo varchar(50) NOT NULL,
+    descricao text,
+    orcamento_id BIGINT(20) NOT NULL,
+
+    FOREIGN KEY(orcamento_id) REFERENCES orcamento(id),
+    data_criacao datetime NOT NULL default CURRENT_DATE,
+    data_update datetime NOT NULL default CURRENT_DATE,
+    criado_por BIGINT(20),
+    updated_por BIGINT(20)
+);
+
+create table factura_item (
+    id BIGINT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    descricao varchar(50) NOT NULL,
+    quantidade INTEGER NOT NULL default 0,
+    valor DECIMAL(10,0) NOT NULL,
+    data datetime NOT NULL default CURRENT_DATE
+    iva DECIMAL(10, 2) NOT NULL DEFAULT 0,
+    factura_id BIGINT(20) NOT NULL,
+    FOREIGN KEY(factura_id) REFERENCES factura(id),
+
+    data_criacao datetime NOT NULL default CURRENT_DATE,
+    data_update datetime NOT NULL default CURRENT_DATE,
+    criado_por BIGINT(20),
+    updated_por BIGINT(20)
+);
+
+create table factura_anexos (
+    id BIGINT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    titulo varchar(50) NOT NULL,
+    path text NOT NULL,
+    factura_id BIGINT(20) NOT NULL,
+    FOREIGN KEY(factura_id) REFERENCES factura(id),
+
+    data_criacao datetime NOT NULL default CURRENT_DATE,
+    data_update datetime NOT NULL default CURRENT_DATE,
+    criado_por BIGINT(20),
+    updated_por BIGINT(20)
+);
+
