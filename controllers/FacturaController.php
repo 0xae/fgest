1464ea784e5cf30a50a0c806090c63a99d0d8969
Ayrton Gomes <com.ayrton@gmail.com>
2017-08-24 19:50:24 -0100

    working on stuff

--- controllers/FacturaController.php
+++ controllers/FacturaController.php
@@ -12,13 +12,11 @@ use yii\filters\VerbFilter;
 /**
  * FacturaController implements the CRUD actions for Factura model.
  */
-class FacturaController extends Controller
-{
+class FacturaController extends Controller {
     /**
      * @inheritdoc
      */
-    public function behaviors()
-    {
+    public function behaviors() {
         return [
             'verbs' => [
                 'class' => VerbFilter::className(),
@@ -33,8 +31,7 @@ class FacturaController extends Controller
      * Lists all Factura models.
      * @return mixed
      */
-    public function actionIndex()
-    {
+    public function actionIndex() {
         $dataProvider = new ActiveDataProvider([
             'query' => Factura::find(),
         ]);
@@ -49,8 +46,7 @@ class FacturaController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionView($id)
-    {
+    public function actionView($id) {
         return $this->render('view', [
             'model' => $this->findModel($id),
         ]);
@@ -61,8 +57,7 @@ class FacturaController extends Controller
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
-    public function actionCreate()
-    {
+    public function actionCreate() {
         $model = new Factura();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
@@ -80,8 +75,7 @@ class FacturaController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionUpdate($id)
-    {
+    public function actionUpdate($id) {
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
@@ -99,10 +93,8 @@ class FacturaController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionDelete($id)
-    {
+    public function actionDelete($id) {
         $this->findModel($id)->delete();
-
         return $this->redirect(['index']);
     }
 
@@ -113,8 +105,7 @@ class FacturaController extends Controller
      * @return Factura the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($id)
-    {
+    protected function findModel($id) {
         if (($model = Factura::findOne($id)) !== null) {
             return $model;
         } else {
